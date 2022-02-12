c018ceb2b0a8f11e89c8383a002ee7ea82b55e3c
janimeister <janiheikkinen94@gmail.com>
2022-02-12 18:42:25 +0200

    changed bootstrap dividers to primeng dividers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RsvpComponent } from './rsvp/rsvp.component';
 import { GettingThereComponent } from './getting-there/getting-there.component';
 import { StayingComponent } from './staying/staying.component';
 import { GiftingComponent } from './gifting/gifting.component';
+import {DividerModule} from 'primeng/divider';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { GiftingComponent } from './gifting/gifting.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    TabMenuModule
+    TabMenuModule,
+    DividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
